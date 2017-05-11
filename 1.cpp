884fc935938a71c732abc6eb33d137318189bbbe
Yuuoniy <linmiaoqian@163.com>
2017-05-11 09:48:38 +0800

    add new recipes

---
+++ 1.cpp
@@ -0,0 +1,16 @@
+/*  1.cpp
+*  1.cpp
+* @Author: Lin Miaoqian
+* @Date:   2017-04-20 14:05:13
+* @Last Modified by:   Lin Miaoqian
+* @Last Modified time: 2017-04-20 14:05:40
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
